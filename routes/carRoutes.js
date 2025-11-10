c7b643a55f364197b353c64426454bfd99a8ac30
Chandan <cb623415@gmail.com>
2025-11-11 03:00:24 +0530

    new checxk

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -25,7 +25,7 @@ router.post(
 router.get("/", getAllCars);       // GET all
 router.get("/:id",getCarById);       
 
-router.get("/userr/:userId", getCarsByUserId);
+router.get("/user/:userId", getCarsByUserId);
 
 
 router.put("/:id", protect, upload.fields([
