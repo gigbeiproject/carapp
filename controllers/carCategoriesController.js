7c8eb99c984a6d94c9dfaff0c1ab6855f60bedc5
Chandan <cb623415@gmail.com>
2025-11-21 15:34:08 +0530

    cccxh

--- controllers/carCategoriesController.js
+++ controllers/carCategoriesController.js
@@ -37,7 +37,7 @@ exports.getCarsWithCategory = async (req, res) => {
 
     let query = `
       SELECT 
-        cars.id AS carId,
+        cars.id AS id,
         cars.userId,
         cars.title,
         cars.city,
