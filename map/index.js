25d491e938de669983de998611c009f228cfdf30
Chandan <cb623415@gmail.com>
2025-12-26 13:12:56 +0530

    change api

--- map/index.js
+++ map/index.js
@@ -103,7 +103,7 @@ app.use("/api/notification", notifications);
 app.use("/admin", UserRouteadmin);
 
 app.get("/", (req, res) => {
-  res.send("Backend is running by chandann!");
+  res.send("Backend is running by chandannnn !");
 });
 
 app.get("/test", (req, res) => {
