dacc81b8b861fc02c4485fd85b783ca592fc6846
Chandan <cb623415@gmail.com>
2026-03-11 18:49:27 +0530

    start booking fix api

--- routes/host/hostBookingStartRoutes.js
+++ routes/host/hostBookingStartRoutes.js
@@ -10,7 +10,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 // ✅ Routes
-router.post("/start-booking", upload.array("files", 10), startBooking);
+router.post("/start-booking", upload.any(), startBooking);
 router.post("/complete-booking", upload.array("files", 10), completeBooking);
 
 module.exports = router;
